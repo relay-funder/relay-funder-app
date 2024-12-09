c3ab5d7b8d15074184781efbeaec569f4aa71358
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-12-09 23:31:43 +0530

    feat: add celo network

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,10 +1,11 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { sepolia, celoAlfajores, celo } from 'wagmi/chains'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia], // adjust chains as needed
+  chains: [celoAlfajores, sepolia, celo], // adjust chains as needed
   transports: {
-    [mainnet.id]: http(),
+    [celoAlfajores.id]: http(),
     [sepolia.id]: http(),
+    [celo.id]: http(),
   },
 }) 
