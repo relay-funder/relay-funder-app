a89d578eaa796ec5902a72664fa8d899283015b0
0xShikhar <shikhar@iiitmanipur.ac.in>
2025-01-08 22:02:47 +0530

    fix

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,2 +1,1 @@
-// const adminAddress = process.env.NEXT_PUBLIC_ADMIN_ADDRESS;
-export const adminAddress = "0x30BCD2e90B3C05e54446568d823408B2ddfa7A01";
+export const adminAddress = process.env.NEXT_PUBLIC_ADMIN_ADDRESS;
