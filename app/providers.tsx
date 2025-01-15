62d43fe3c0ff6885aad0ca9342b4aee109ea1cc0
J△MΞS <jamespfarrell@gmail.com>
2025-01-15 12:28:12 +0100

    feat: fix the admin approve function
    it was previously using view for windown.ethereum and creating
    the signer from there. Changed to use Privy for these and handle
    several walelts, as one would expect from Privy

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,63 +2,58 @@
 
 import { PrivyProvider } from '@privy-io/react-auth';
 import { ReactNode } from 'react';
-import { createConfig, http, WagmiProvider } from 'wagmi'
-import { sepolia, celoAlfajores, mainnet } from 'wagmi/chains'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CollectionProvider } from '@/contexts/CollectionContext'
-import { SidebarProvider } from '@/contexts/SidebarContext'
+import { http } from 'wagmi';
+import { WagmiProvider, createConfig } from '@privy-io/wagmi';
+import { sepolia, celoAlfajores, mainnet } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CollectionProvider } from '@/contexts/CollectionContext';
+import { SidebarProvider } from '@/contexts/SidebarContext';
 
 const config = createConfig({
   chains: [sepolia, celoAlfajores, mainnet],
   transports: {
     [celoAlfajores.id]: http(),
     [sepolia.id]: http(),
-    [mainnet.id]: http()
-  }
-})
+    [mainnet.id]: http(),
+  },
+});
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <WagmiProvider config={config}>
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
+      config={{
+        appearance: {
+          accentColor: '#44d29b',
+          theme: '#FFFFFF',
+          showWalletLoginFirst: false,
+          logo: 'https://auth.privy.io/logos/privy-logo.png',
+          walletChainType: 'ethereum-only',
+        },
+        loginMethods: ['email', 'wallet', 'google'],
+        fundingMethodConfig: {
+          moonpay: {
+            useSandbox: true,
+          },
+        },
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+          requireUserPasswordOnCreate: false,
+        },
+        mfa: {
+          noPromptOnMfaRequired: false,
+        },
+      }}
+    >
       <QueryClientProvider client={queryClient}>
-        <PrivyProvider
-          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
-          config={{
-            "appearance": {
-              "accentColor": "#44d29b",
-              "theme": "#FFFFFF",
-              "showWalletLoginFirst": false,
-              "logo": "https://auth.privy.io/logos/privy-logo.png",
-              "walletChainType": "ethereum-only"
-            },
-            "loginMethods": [
-              "email",
-              "wallet",
-              "google"
-            ],
-            "fundingMethodConfig": {
-              "moonpay": {
-                "useSandbox": true
-              }
-            },
-            "embeddedWallets": {
-              "createOnLogin": "users-without-wallets",
-              "requireUserPasswordOnCreate": false
-            },
-            "mfa": {
-              "noPromptOnMfaRequired": false
-            }
-          }}
-        >
+        <WagmiProvider config={config}>
           <CollectionProvider>
-            <SidebarProvider>
-              {children}
-            </SidebarProvider>
+            <SidebarProvider>{children}</SidebarProvider>
           </CollectionProvider>
-        </PrivyProvider>
+        </WagmiProvider>
       </QueryClientProvider>
-    </WagmiProvider>
+    </PrivyProvider>
   );
-} 
+}
