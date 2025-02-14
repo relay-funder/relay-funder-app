ed060b8719fd62224513f53cc53c7e3794854127
Prajjawalk <prajjawalkhandelwal@gmail.com>
2025-02-14 23:39:19 +0530

    fix: campaign rounds fetch

--- app/api/campaigns/round/[id]/route.ts
+++ app/api/campaigns/round/[id]/route.ts
@@ -3,15 +3,19 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: Promise<{ id: number }> }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const campaignId = (await params).id; // Get the ID from the query parameters
 
   try {
     const roundCampaigns = await prisma.roundCampaigns.findMany({
-      where: { campaignId },
+      where: { campaignId: parseInt(campaignId) },
       include: { round: true }, // Include round details
     });
+
+    if (roundCampaigns.length == 0) {
+      return NextResponse.json({ rounds: [] }, { status: 200 });
+    }
 
     const rounds = roundCampaigns.map((rc) => ({
       id: rc.roundId,
@@ -20,7 +24,7 @@ export async function GET(
 
     return NextResponse.json({ rounds }, { status: 200 });
   } catch (error) {
-    console.error("Failed to fetch rounds for campaign:", error);
+    console.error("Failed to fetch rounds for campaign:", error.stack);
     return NextResponse.json(
       { error: "Failed to fetch rounds" },
       { status: 500 }
