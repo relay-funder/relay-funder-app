33ed6e380b923024bf87d54e3195dc0cd5926eb3
Shikhar Singh <0xshikhar@gmail.com>
2025-04-04 18:43:22 +0530

    feat: add qf contracts

--- lib/constant.ts
+++ lib/constant.ts
@@ -6,6 +6,13 @@ export const adminAddress = process.env.NEXT_PUBLIC_PLATFORM_ADMIN;
 
 export const USDC_ADDRESS = process.env.NEXT_PUBLIC_PLEDGE_TOKEN;
 
+export const NFT_METADATA = "0x73258C5813AB9494473d34eD57869531B2dd6ac2";
+export const CAMPAIGN_NFT_FACTORY = "0x41ab8cf665f931B010d13D46b5178551594EE2DA";
+export const AKASHIC_NFT_REGISTRY = "0x41b40AB343E607e59de62930D726A324B3b7cf63";
+
+export const ALLO_ADDRESS = "0x1133eA7Af70876e64665ecD07C0A0476d09465a1";
+export const KICKSTARTER_QF_ADDRESS = "0x331e239848571d98B917982c16562b6E29DD91C5";
+
 // Mock user campaigns - In production, this would come from API
 export const MOCK_USER_CAMPAIGNS = [
   {
@@ -73,11 +80,6 @@ export const MOCK_ROUNDS: Round[] = [
     logoUrl: "/akashic-logo.png",
   },
 ];
-
-
-export const NFT_METADATA = "0x73258C5813AB9494473d34eD57869531B2dd6ac2";
-export const CAMPAIGN_NFT_FACTORY = "0x41ab8cf665f931B010d13D46b5178551594EE2DA";
-export const AKASHIC_NFT_REGISTRY = "0x41b40AB343E607e59de62930D726A324B3b7cf63";
 
 export const collections = [
 	{ id: 1, name: "Collection 1", initial: "1" },
