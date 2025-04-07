7e64b16584aae11d1e0ea4d135bab67fae42fea0
Shikhar Singh <0xshikhar@gmail.com>
2025-04-07 23:03:08 +0530

    feat: create custom pool

---
+++ prisma/migrations/20250408183517_update_rounds/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Round` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Round" DROP COLUMN "status";
