39677f663a9f09d0542d1d3155d8e7e8e6aa46a1
Shikhar Singh <0xshikhar@gmail.com>
2025-04-29 19:58:23 +0530

    feat: add username feild in profile page

--- app/api/users/me/route.ts
+++ app/api/users/me/route.ts
@@ -22,6 +22,7 @@ export async function GET(request: NextRequest) {
                 createdAt: true,
                 updatedAt: true,
                 isKycCompleted: true,
+                username: true,
             }
         });
 
