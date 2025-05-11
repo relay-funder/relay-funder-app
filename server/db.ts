26ae721b19c765cff5d0cbb6163d1ae4b7cf970a
jsos <183998181+js-0s@users.noreply.github.com>
2025-05-11 20:28:44 +0200

    - prettierignore docker-compose.yml (double-quotes are more correct there) - add a env in dockerfile that reduces the compile times of nextjs - add a server/lib (needs to be applied everywhere where prisma is   imported directly) for db-access

---
+++ server/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+const createPrismaClient = () =>
+  new PrismaClient({
+    log: ['error'],
+  });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
