1557e1146a4c4dc827f3db76e941a73009a3f3fd
jsos <183998181+js-0s@users.noreply.github.com>
2025-05-12 15:59:53 +0200

    fix: sidebar auto-hidden on mobile
    problem: the debounce does not handle the mouseEnter=mouseLeave event on
    mobile
    solution: use a ref in the sidebar context and do not hide when the event
    occurs in less than 1000ms from the show, that time is ignored when the
    mouse moves inbetween
    
    - remove migrated files

--- components/page/sidebar.tsx
+++ components/page/sidebar.tsx
@@ -9,7 +9,7 @@ import { PageNavMenuFeatures } from './nav-menu-features';
 import { PageNavMenuUser } from './nav-menu-user';
 import { transition } from './sidebar-constants';
 export const PageSidebar = () => {
-  const { isOpen, setIsOpen } = useSidebar();
+  const { isOpen, show, hide, move } = useSidebar();
 
   return (
     <div className={cn(isOpen ? 'h-screen' : 'h-[100px] md:h-screen')}>
@@ -20,8 +20,9 @@ export const PageSidebar = () => {
           transition,
           isOpen ? 'h-full w-[240px]' : 'h-[100px] w-[70px] md:h-full',
         )}
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}
+        onMouseEnter={show}
+        onMouseLeave={hide}
+        onMouseMove={move}
       >
         <div className={cn('flex flex-col', 'h-screen')}>
           <PageNavMenuIcon />
--- contexts/SidebarContext.tsx
+++ contexts/SidebarContext.tsx
@@ -5,35 +5,57 @@ import {
   createContext,
   useContext,
   useState,
-  useRef,
   useCallback,
   useMemo,
+  useRef,
   ReactNode,
 } from 'react';
 
 interface SidebarContextType {
   isOpen: boolean;
-  setIsOpen: (open: boolean) => void;
+  show: () => void;
+  hide: () => void;
+  move: () => void;
 }
 
 const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+// ms that need to pass until a close followed by a open actually is executed
+const minToggleTime = 1000;
 
 export function SidebarProvider({ children }: { children: ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
-  const lastOpen = useRef(0);
-  const setOpen = useCallback(
-    (newState: boolean) => {
-      setIsOpen(newState);
-    },
-    [setIsOpen],
-  );
-  const setOpenDebounced = useDebouncedCallback(setOpen, 200);
+  const refToggleTime = useRef<number>(0);
+  // a debounce-like functionality that works around the fact that
+  // mobile devices do trigger onMouseEnter and onMouseLeave for a touch
+  // and never trigger a onMouseMove. Therefore if we receive a hide event
+  // right after a show event, we ignore the hide-event. If the mouse-cursor
+  // moves between show & hide, that indicates its a desktop user
+  const show = useCallback(() => {
+    setIsOpen(true);
+    refToggleTime.current = Date.now();
+  }, [setIsOpen]);
+  const hide = useCallback(() => {
+    if (Date.now() - refToggleTime.current < minToggleTime) {
+      return;
+    }
+    setIsOpen(false);
+    refToggleTime.current = 0;
+  }, [setIsOpen]);
+  const move = useCallback(() => {
+    // when we register a move, the user is on a desktop, so we can
+    // safely ignore the boundary for the close as we receive a onMouseLeave
+    // at the appropriate time.
+    // This wont happen on mobile.
+    refToggleTime.current = 0;
+  }, []);
   const value = useMemo(() => {
     return {
+      show,
+      hide,
+      move,
       isOpen,
-      setIsOpen: setOpenDebounced,
     };
-  }, [isOpen, setOpenDebounced]);
+  }, [isOpen, show, hide, move]);
   return (
     <SidebarContext.Provider value={value}>{children}</SidebarContext.Provider>
   );
