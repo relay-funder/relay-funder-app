142c45e188efe89de5d371f8cd66858d0893fbd6
Lukas <profiles@goodthings.dev>
2025-05-12 19:03:28 +0100

    fix(sentry-config): optimize params for production

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,11 +8,8 @@ import * as Sentry from '@sentry/nextjs';
 if (process.env.SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-
-    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: 1,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
+    tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
+    debug: process.env.NODE_ENV !== 'production',
+    environment: process.env.NODE_ENV,
   });
 }
