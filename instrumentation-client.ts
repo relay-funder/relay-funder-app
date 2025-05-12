16ea4240d421c0d95b6705d7729790d64999019d
Lukas <profiles@goodthings.dev>
2025-05-12 18:42:03 +0100

    chore(lint): prettier write

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,7 +2,7 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 if (process.env.SENTRY_DSN) {
   Sentry.init({
