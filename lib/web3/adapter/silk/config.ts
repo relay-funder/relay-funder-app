bf51ac647b225ca7fb6363cc7efd0e94a4b2826c
jsos <183998181+js-0s@users.noreply.github.com>
2025-06-30 16:54:16 +0200

    feat(dev): optimize wagmi,ethers,veim loading
    - refactor all component imports to request '@/lib/web3' for required methods
      this gives us the opportunity to handle quirks that may be required
      with certain wallet adapters and makes it obvious which pieces are
      actually used from different libraries
    - move viem.parseUnits to ethers.parseUnits, move viem.formatUnits to ethers.formatUnits
    - move lib/qfInteractions types to lib/qf/types
    - dummy adapter which declares the types and interfaces used in the library
      and returns dummy data. this is useful for testing imports and how they
      affect the performance
    
    compared to before, 10k modules where loaded to compile '/' which took
    close to a minute, every component-change(hot-reload) took at least 30s.
    with the dummy adapter, this is down to 1522 modules and usually around 1s
    for hotreloading
    
    importing viem/ethers types does not change the compile time
    
    with silk enabled (and optimized package.json to ensure only one version
    of heavy imports is in the project) its still 8255 modules but changing a
    component now only takes 3-10s (still too slow, but dummy adapter is a
    easy way out) the reason there seems to be wagmi

---
+++ lib/web3/adapter/silk/config.ts
@@ -0,0 +1,4 @@
+export { chainConfig } from '@/lib/web3/config/chain';
+export { config as wagmiConfig } from '@/lib/web3/config/wagmi';
+
+//export * from '../dummy/config';
---
+++ lib/web3/adapter/silk/wagmi.ts
@@ -0,0 +1,13 @@
+// wagmi/core  1837->2522 + 500ms
+
+// ~680 modules
+export { readContract, createConfig } from '@wagmi/core';
+// ~1250 modules
+export {
+  useConnectorClient,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+  useDeployContract,
+  useConfig,
+  useChainId,
+} from 'wagmi';
