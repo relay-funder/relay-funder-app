9ba433f11a3a46e73ef6e2a6ab540c0cea4ab02b
jsos <183998181+js-0s@users.noreply.github.com>
2025-08-02 00:06:01 +0200

    feat(donation): refactored ui for project-info
    - matches the data from the cards

--- components/project-info.tsx
+++ components/project-info.tsx
@@ -1,90 +1,97 @@
 'use client';
 
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { Badge } from './ui/badge';
-import { Card } from './ui/card';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Card, CardContent } from '@/components/ui';
+import { ZeroFeePromise } from '@/components/zero-fee-promise';
 import { useCampaign } from '@/lib/hooks/useCampaigns';
-import { useMemo } from 'react';
-import { CampaignImage, Payment } from '@/types/campaign';
+import { GetCampaignResponse } from '@/lib/api/types';
+import { FormattedDate } from './formatted-date';
+import { UserInlineName } from './user/inline-name';
+import { UserInlineBadges } from './user/inline-badges';
+import { useCampaignStats } from '@/hooks/use-campaign-stats';
+import { CampaignProgress } from './campaign/progress';
+import { CampaignDaysLeftBlock } from './campaign/days-left-block';
+import { CampaignMainImage } from './campaign/main-image';
+import { CampaignLoading } from './campaign/loading';
+
 interface ProjectInfoProps {
   slug: string;
 }
 
 export default function ProjectInfo({ slug }: ProjectInfoProps) {
-  const { data: campaign, isPending } = useCampaign(slug);
-  const amountRaised = useMemo(() => {
-    if (!Array.isArray(campaign?.payments)) {
-      return 0;
-    }
-    return campaign.payments.reduce((accumulator: number, payment: Payment) => {
-      if (isNaN(Number(payment.amount))) {
-        return accumulator;
-      }
-      return accumulator + Number(payment.amount);
-    }, 0);
-  }, [campaign]);
+  const { data, isPending } = useCampaign(slug);
+  const { campaign } = data ?? ({} as GetCampaignResponse);
+  const { contributorCount } = useCampaignStats({
+    slug,
+  });
+  const hasContributors = contributorCount > 0;
+  const lastConfirmed = campaign?.paymentSummary?.lastConfirmed;
+  const lastPending = campaign?.paymentSummary?.lastPending;
+
   if (isPending || !campaign) {
-    return null;
+    return <CampaignLoading expectItemCount={1} />;
   }
-  const mainImage =
-    campaign.images?.find((img: CampaignImage) => img.isMainImage) ||
-    campaign.images?.[0];
+
   return (
-    <div className="space-y-6">
+    <div className="space-y-4">
       <Card className="w-fit overflow-hidden">
-        <Image
-          src={
-            mainImage
-              ? mainImage.imageUrl
-              : '/placeholder.svg?height=300&width=600'
-          }
-          alt={`${campaign.title} illustration`}
-          width={600}
-          height={300}
-          className="object-cover"
-          loading="lazy"
-        />
-      </Card>
-
-      <div className="flex gap-2">
-        <Badge className="bg-indigo-600">Verified</Badge>
-        {/* <Badge variant="secondary">VOUCHED</Badge> */}
-      </div>
-
-      <div className="space-y-4">
-        <div className="flex items-center gap-3">
-          <Avatar>
-            <AvatarImage src="/placeholder.svg" />
-            <AvatarFallback>
-              {campaign.creatorAddress.slice(0, 2).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-          <h1 className="text-xl font-semibold text-pink-500">
-            {campaign.title}
-          </h1>
+        <div className="relative">
+          <CampaignMainImage campaign={campaign} />
         </div>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="flex justify-between gap-3">
+              <h1 className="text-xl font-semibold text-pink-500">
+                {campaign.title}
+              </h1>
+              <div className="">
+                By
+                <UserInlineBadges user={campaign?.creator} />
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <CampaignProgress campaign={campaign} className="flex-grow" />
+              <CampaignDaysLeftBlock
+                campaign={campaign}
+                className="align-middle"
+              />
+            </div>
 
-        <div>
-          <div className="text-sm text-muted-foreground">Raised</div>
-          <div className="text-2xl font-bold">
-            ${amountRaised.toLocaleString()}
+            <div className="items-top flex flex-row gap-3">
+              {hasContributors && (
+                <div>
+                  <div className="text-sm text-muted-foreground">
+                    {contributorCount.toLocaleString()} Contributors
+                  </div>
+
+                  {lastConfirmed && (
+                    <div className="text-sm font-bold">
+                      <UserInlineName user={lastConfirmed.user} />
+                      donated {lastConfirmed.amount.toLocaleString()}{' '}
+                      {lastConfirmed.token ?? '$'}{' '}
+                      {lastConfirmed.date && (
+                        <FormattedDate date={lastConfirmed.date} />
+                      )}
+                    </div>
+                  )}
+                  {lastPending && (
+                    <div className="text-sm font-bold text-muted-foreground">
+                      <UserInlineName user={lastPending.user} />
+                      donating {lastPending.amount.toLocaleString()}{' '}
+                      {lastPending.token ?? '$'}{' '}
+                      {lastPending.date && (
+                        <FormattedDate date={lastPending.date} />
+                      )}
+                    </div>
+                  )}
+                </div>
+              )}
+              <ZeroFeePromise />
+            </div>
+
+            <p className="text-muted-foreground">{campaign.description}</p>
           </div>
-        </div>
-
-        <p className="text-muted-foreground">{campaign.description}</p>
-
-        <div className="space-y-4 rounded-lg bg-slate-50 p-4">
-          <h3 className="font-semibold">100% goes to the project always.</h3>
-          <p className="text-muted-foreground">
-            Every donation is peer-to-peer, with no fees and no middlemen.
-          </p>
-          <Link href="#" className="text-sm text-pink-500 hover:underline">
-            Learn about our zero-fee policy →
-          </Link>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
