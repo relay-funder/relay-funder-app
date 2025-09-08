226ccb6fd3b761d5458f22683a70fd5d47ab46a7
jsos <183998181+js-0s@users.noreply.github.com>
2025-09-08 21:34:35 +0200

    fix(admin): pass admin-state for GET /rounds/id
    - correct response after modifying rounds (approve etc)

--- app/api/rounds/[id]/route.ts
+++ app/api/rounds/[id]/route.ts
@@ -2,21 +2,22 @@ import { db } from '@/server/db';
 import { ApiNotFoundError, ApiParameterError } from '@/lib/api/error';
 import { response, handleError } from '@/lib/api/response';
 import { RoundsWithIdParams } from '@/lib/api/types';
-import { checkAuth } from '@/lib/api/auth';
+import { checkAuth, isAdmin } from '@/lib/api/auth';
 import { getRound } from '@/lib/api/rounds';
 import { auth } from '@/server/auth';
 
 export async function GET(req: Request, { params }: RoundsWithIdParams) {
   try {
     // public endpoint
+    // when signed in, session & admin declare how much relation data is fetched
+    // default is only approved
+    // as admin, get any state
+    // as user only get any-state when campaign is created by user
     const session = await auth();
+    const admin = await isAdmin();
 
     const id = (await params).id;
-    const round = await getRound(
-      parseInt(id),
-      undefined,
-      session?.user.address,
-    );
+    const round = await getRound(parseInt(id), admin, session?.user.address);
 
     return response({ round });
   } catch (error: unknown) {
