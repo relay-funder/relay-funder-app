86e4ef8885fc59cb09f3eb94813203ec48b0a119
jsos <183998181+js-0s@users.noreply.github.com>
2025-09-14 22:10:23 +0200

    feat(api): admin to update user flags
    - api for listing users, updating string fields and flags

---
+++ app/api/admin/users/[id]/route.ts
@@ -0,0 +1,82 @@
+import { checkAuth } from '@/lib/api/auth';
+import { ApiAuthNotAllowed, ApiNotFoundError } from '@/lib/api/error';
+import { response, handleError } from '@/lib/api/response';
+import {
+  GetUserResponse,
+  PatchUserRouteBodySchema,
+  UserWithAddressParams,
+} from '@/lib/api/types/admin';
+import {
+  getUser,
+  updateUserBio,
+  updateUserEmail,
+  updateUserKycStatus,
+  updateUserNames,
+  updateUserRecipientWallet,
+} from '@/lib/api/user';
+
+export async function GET(req: Request, { params }: UserWithAddressParams) {
+  try {
+    await checkAuth(['admin']);
+    const { address } = await params;
+    const instance = await getUser(address);
+    if (!instance) {
+      throw new ApiNotFoundError('User not found');
+    }
+
+    return response({
+      user: instance,
+    } as GetUserResponse);
+  } catch (error: unknown) {
+    return handleError(error);
+  }
+}
+
+export async function PATCH(req: Request, { params }: UserWithAddressParams) {
+  try {
+    await checkAuth(['admin']);
+    const { address } = await params;
+    const {
+      email,
+      username,
+      firstName,
+      lastName,
+      bio,
+      recipientWallet,
+      isKycCompleted,
+    } = PatchUserRouteBodySchema.parse(await req.json());
+
+    const instance = await getUser(address);
+    if (!instance) {
+      throw new ApiNotFoundError('User not found');
+    }
+    if (!instance.featureFlags.includes('USER_MODERATOR')) {
+      throw new ApiAuthNotAllowed('Admin needs USER_MODERATOR flag');
+    }
+    if (email) {
+      await updateUserEmail(address, email);
+    }
+    if (
+      typeof username !== 'undefined' ||
+      typeof firstName !== 'undefined' ||
+      typeof lastName !== 'undefined'
+    ) {
+      await updateUserNames(address, username, firstName, lastName);
+    }
+    if (typeof bio !== 'undefined') {
+      await updateUserBio(address, bio);
+    }
+    if (typeof recipientWallet !== 'undefined') {
+      await updateUserRecipientWallet(address, recipientWallet);
+    }
+    if (typeof isKycCompleted !== 'undefined') {
+      await updateUserKycStatus(address, isKycCompleted);
+    }
+    const updatedInstance = await getUser(address);
+    return response({
+      user: updatedInstance,
+    } as GetUserResponse);
+  } catch (error: unknown) {
+    return handleError(error);
+  }
+}
---
+++ prisma/migrations/20250914195409_add_humanity_score_to_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "humanityScore" INTEGER NOT NULL DEFAULT 0;
