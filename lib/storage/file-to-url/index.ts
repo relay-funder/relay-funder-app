ed11290aaa9bb34e980ed6ea141f40aa88fb1ca8
Lukas <profiles@goodthings.dev>
2025-09-17 19:45:47 +0100

    feat(uploads): phase out cloudinary

--- lib/storage/file-to-url/index.ts
+++ lib/storage/file-to-url/index.ts
@@ -1,12 +1,8 @@
 import { FILE_STORAGE_PROVIDER } from '@/lib/constant';
-import { fileToUrl as cloudinaryFileToUrl } from './cloudinary';
 import { fileToUrl as localFileToUrl } from './local';
 import { fileToUrl as pinataFileToUrl } from './pinata';
 
 export async function fileToUrl(file: File): Promise<string> {
-  if (FILE_STORAGE_PROVIDER === 'CLOUDINARY') {
-    return cloudinaryFileToUrl(file);
-  }
   if (FILE_STORAGE_PROVIDER === 'PINATA') {
     return pinataFileToUrl(file);
   }
@@ -14,6 +10,6 @@ export async function fileToUrl(file: File): Promise<string> {
     return localFileToUrl(file);
   }
   throw new Error(
-    `Bad FILE_STORAGE_PROVIDER (${FILE_STORAGE_PROVIDER}) configured`,
+    `Bad FILE_STORAGE_PROVIDER (${FILE_STORAGE_PROVIDER}) configured. Supported: 'PINATA', 'LOCAL'`,
   );
 }
