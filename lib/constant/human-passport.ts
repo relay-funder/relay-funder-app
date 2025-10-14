02bd1a59d5fcd5d45929066fb99b78dfd8b26c53
hussmartinez <dev@hussmartinez.xyz>
2025-10-14 17:22:50 +0700

    feat: human passport constants

---
+++ lib/constant/human-passport.ts
@@ -0,0 +1,5 @@
+export const PASSPORT_API_BASE_URL = 'https://api.passport.xyz';
+export const PASSPORT_API_KEY = process.env.PASSPORT_API_KEY || '';
+export const PASSPORT_SCORER_ID = process.env.PASSPORT_SCORER_ID || '';
+
+export const PASSPORT_SCORE_THRESHOLD = 20;
