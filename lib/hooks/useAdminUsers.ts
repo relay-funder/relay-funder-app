d32fcc4e0d7737087e43c25f7f61524fa7bea8f1
jsoss <230627306+joerg-oss@users.noreply.github.com>
2025-10-18 00:08:10 +0200

    fix(admin): ensure reload after user modification
    - use correct invalidation key

--- lib/hooks/useAdminUsers.ts
+++ lib/hooks/useAdminUsers.ts
@@ -10,6 +10,7 @@ import type {
   PatchUserRouteBody,
 } from '@/lib/api/types/admin';
 import { PaginatedResponse } from '@/lib/api/types';
+import { ADMIN_USER_OVERVIEW_QUERY_KEY } from '@/lib/hooks/useAdminUserOverview';
 
 export const ADMIN_USERS_QUERY_KEY = 'admin_users';
 export const ADMIN_USER_QUERY_KEY = 'admin_user';
@@ -233,6 +234,9 @@ export function useUpdateAdminUser() {
       queryClient.invalidateQueries({ queryKey: [ADMIN_USERS_QUERY_KEY] });
       queryClient.invalidateQueries({
         queryKey: [ADMIN_USER_QUERY_KEY, variables.address],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [ADMIN_USER_OVERVIEW_QUERY_KEY, variables.address],
       });
     },
   });
